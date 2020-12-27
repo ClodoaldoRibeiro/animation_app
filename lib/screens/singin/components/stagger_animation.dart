343636d0d24350a7cc298b1ae606c75ec8518184
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2020-12-26 23:42:57 -0300

    Update animation
    Ajustar animações.

--- lib/screens/singin/components/stagger_animation.dart
+++ lib/screens/singin/components/stagger_animation.dart
@@ -3,32 +3,47 @@ import 'package:flutter/material.dart';
 class StaggerAnimation extends StatelessWidget {
   final AnimationController controller;
 
-  StaggerAnimation({this.controller});
+  final Animation<double> buttonSqueeze;
+
+  StaggerAnimation({this.controller})
+      : buttonSqueeze = Tween(begin: 320.0, end: 60.0).animate(
+            CurvedAnimation(parent: controller, curve: Interval(0.0, 0.150)));
 
   Widget _buildAnimation(BuildContext context, Widget child) {
     return Padding(
       padding: EdgeInsets.only(bottom: 40),
       child: InkWell(
           onTap: () {
-            print("Clicou no botão Sign In ás ${DateTime.now()}");
+            controller.forward();
           },
           child: Container(
-            width: 320,
+            width: buttonSqueeze.value,
             height: 60,
             alignment: Alignment.center,
             decoration: BoxDecoration(
                 color: Color.fromRGBO(247, 64, 106, 1.0),
                 borderRadius: BorderRadius.all(Radius.circular(30.0))),
-            child: Text(
-              "Sign in",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w300,
-                  letterSpacing: 0.3),
-            ),
+            child: _buildInside(context),
           )),
     );
+  }
+
+  Widget _buildInside(BuildContext context) {
+    if (buttonSqueeze.value > 75) {
+      return Text(
+        "Sign in",
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w300,
+            letterSpacing: 0.3),
+      );
+    } else {
+      return CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+        strokeWidth: 1.0,
+      );
+    }
   }
 
   @override
